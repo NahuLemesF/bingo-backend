d336f2646d34129b9b56dec0acc261920268f9f8
Nahuel <nahulem@gmail.com>
2024-11-23 11:28:54 -0300

    Se implementan rutas del juego.
    - Se crea el archivo gameRoutes.js
    - Se importa y se asignan rutas en server.js

---
+++ routes/gameRoutes.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import { startGame, assignCardToPlayer } from '../controllers/gameController.js';
+
+const router = express.Router(); // Enrutador de express
+
+// Ruta para iniciar el juego
+router.post('/start-game', startGame);
+
+// Ruta para asignar tarjetas
+router.post('/assign-cards/:gameId', async (req, res) => {
+    try {
+        // Obtenemos el ID del Juego de los parametros de la URL
+        const { gameId } = req.params;
+        
+        // Llamamos a la función que inicia el juego y devuelve las tarjetas asignadas
+        const assignedCards = await assignCardToPlayer(gameId)
+        
+        // Retornamos el resultado al cliente con un código de estado 200 (OK) y el mensaje de éxito
+        res.status(200).json({ message: 'Juego iniciado y tarjetas asignadas', assignedCards });
+    } catch (error) {
+        // En caso de error, retornamos un código de estado 500 (Internal Server Error) y el mensaje de error
+        console.error(error);
+        res.status(500).json({ message: 'Error al iniciar el juego' });
+    }
+})
+
+// Exportamos el enrutador para que pueda ser utilizado en otros archivos
+export default router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import gameRoutes from "./routes/gameRoutes.js";
 
 dotenv.config(); 
 
@@ -12,8 +13,9 @@ app.use(express.json()); // Middleware para parsear JSON
 // Conectar a MongoDB
 connectDB();
 
-// Rutas de autenticacion
+// Rutas de autenticacion y de juego
 app.use("/api/auth", authRoutes);
+app.use("/api/game", gameRoutes);
 
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000; //Si no existe el puerto en el deployement, se asigna el 3000
